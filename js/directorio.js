bdc4878ee521dff5b003fde2bd1e3f555447574c
ikermentxaka <164804377+ikermentxaka@users.noreply.github.com>
2026-04-04 10:40:22 +0200

    Añador comentarios directorio.js

--- js/directorio.js
+++ js/directorio.js
@@ -1,6 +1,7 @@
 /*//Este dibuja en el directorio*/
-
+/*fetch es una api para hacer peticiones http, aqui la usamos para obtener el json*/
 fetch('/drops/data/drops.json')
+/*Esta es la estrctura básica en un fetch, habiendo un response y un data*/
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('drops-container');
@@ -9,13 +10,16 @@ fetch('/drops/data/drops.json')
       const card = document.createElement('a');
       card.href = drop.link;
 
+/*innerHtml establece el html de un elemento*/
       card.innerHTML = `
         <div class="card" id="card">
           <img src="../drops/data/${drop.image}" />
           <h2>${drop.title}</h2>
         </div>
       `;
+/*El metodo appendChild inserta un elememto hijo al elemento seleccionado.
 
+En este caso en cada vuelta, se introduce una card.*/
       container.appendChild(card);
     });
   });
