ff5da523ba7e3a3b5e4fb8367c5a508d31ce84a0
ikermentxaka <164804377+ikermentxaka@users.noreply.github.com>
2026-04-05 18:59:14 +0200

    Acceso al visu y funcionalidad

--- pags/entretenimiento/visu3d/js/main.js
+++ pags/entretenimiento/visu3d/js/main.js
@@ -9,18 +9,49 @@ const scene = new THREE.Scene();
 //create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-//Keep track of the mouse position, so we can make the eye move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-
 //Keep the 3D object on a global variable so we can access it later
 let object;
 
 //OrbitControls allow the camera to move around the scene
 let controls;
 
-//Set which object to render
+//Set which object to render first
 let objToRender = 'dino';
+//Funcion para elegir que objeto renderizar
+document.getElementById('btnDino').addEventListener('click', () => {
+  cambioPieza('dino');
+});
+
+document.getElementById('btnEye').addEventListener('click', () => {
+  cambioPieza('eye');
+});
+
+window.cambioPieza = function(nombre){
+  objToRender = nombre;
+  console.log(objToRender);
+  loadModel(nombre);
+}
+
+function loadModel(nombre) {
+  // Eliminar el modelo anterior si existe
+  if (object) {
+    scene.remove(object);
+  }
+
+  loader.load(
+    `./models/${nombre}/scene.gltf`,
+    function (gltf) {
+      object = gltf.scene;
+      scene.add(object);
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
+}
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
@@ -74,26 +105,15 @@ scene.add(bottomLight);
 const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "eye" ? 20 : 7);
 scene.add(ambientLight);
 
-const ambientLightD = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 20 : 7);
-scene.add(ambientLightD);
 
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "dino") {
-  controls = new OrbitControls(camera, renderer.domElement);
+controls = new OrbitControls(camera, renderer.domElement);
   
-}
+
 
 //Render the scene
 function animate() {
   requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
-
-  //Make the eye move
-  if (object && objToRender === "eye") {
-    //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-  }
   renderer.render(scene, camera);
 }
 
@@ -104,11 +124,7 @@ window.addEventListener("resize", function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//add mouse position listener, so we can make the eye move
-document.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-}
+
 
 //Start the 3D rendering
 animate();
